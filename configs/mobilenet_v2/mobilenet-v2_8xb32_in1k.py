40012b3050194844620ccabab606b7e6fe6afc7b
gqw <994321849@qq.com>
2023-07-04 10:17:55 +0800

    bs to 256

--- configs/mobilenet_v2/mobilenet-v2_8xb32_in1k.py
+++ configs/mobilenet_v2/mobilenet-v2_8xb32_in1k.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/mobilenet_v2_1x.py',
-    '../_base_/datasets/mobilenetv2_imagenet_bs32_pil_resize.py.py',
+    '../_base_/datasets/imagenet_bs32_pil_resize_mbv2.py',
     '../_base_/schedules/imagenet_bs256_epochstep.py',
     '../_base_/default_runtime.py'
 ]
